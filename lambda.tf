23ebd32ca159acca841fad7b51f03b450641a288
Andrew Perry <andyjp94@gmail.com>
2019-04-28 08:21:28 +0100

    Fix: Stop zipping the lambda as this is an antipattern
    This step should be done before the terraform is called.
    The archive_file resource is a reluctantly supported feature
    for backwards compatability. However it is recommended to zip
    lambdas prior to calling terraform.

--- lambda.tf
+++ lambda.tf
@@ -2,16 +2,10 @@ data "aws_iam_role" "cloudfront_access" {
   name = "access-cloudfront"
 }
 
-data "archive_file" "source" {
-  type        = "zip"
-  source_file = "${lookup(var.lambda,"file_path", "")}"
-  output_path = ".terraform_tmp/headers.zip"
-}
-
 resource "aws_lambda_function" "security_lambda" {
   provider         = "aws.virginia"
-  filename         = ".terraform_tmp/headers.zip"
   function_name    = "add-security-headers"
+  filename         = "${lookup(var.lambda,"file_path", "")}"
   role             = "${data.aws_iam_role.cloudfront_access.arn}"
   handler          = "${lookup(var.lambda,"handler", "")}"
   source_code_hash = "${data.archive_file.source.output_base64sha256}"
